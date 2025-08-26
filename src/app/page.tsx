98b45c959c01cac1de5a81ca857ff93204823bea
m4r13y <calmarley@outlook.com>
2025-08-26 12:51:47 -0500

    metadata title update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import OptimizedHome from "@/components/optimized-home"
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: 'Affordable Insurance for Families & Businesses | Hawkins Insurance Group',
+  title: 'Hawkins Insurance Group | Affordable Insurance for Families & Businesses',
   description: 'Get expert advice and personalized quotes for health, life, and supplemental insurance. Hawkins Insurance Group helps families and businesses secure their future with trusted solutions.',
   alternates: {
     canonical: '/',
